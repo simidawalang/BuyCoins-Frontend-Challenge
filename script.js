5335116ceae25e5e4f9e3237acbc3969ba3821eb
simidawalang <simidawalang@users.noreply.github.com>
2020-11-28 13:27:41 +0100

    Smaller screens

---
+++ script.js
@@ -0,0 +1,6 @@
+// var menu = document.getElementById('header-container');
+// var hamburger = document.getElementById('hamburger-menu');
+
+// hamburger.addEventListener('click', function(){
+// 	menu.style.display = "block";
+// });
